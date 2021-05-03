3e23767a583c02b29e2e929846fd9b2ec32f91b5
Tran Hung <trhun810@gmail.com>
2021-05-04 02:51:34 +0700

    abc

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-let noOver = document.getElementById('no');
-let yesOver = document.getElementById('yes');
-let answerBox = document.getElementsByClassName('answer');
+let noOver = document.getElementById("no");
+let yesOver = document.getElementById("yes");
+let answerBox = document.getElementsByClassName("answer");
 
-noOver.addEventListener('mouseover', reverseAns);
-yesOver.addEventListener('click', yesss);
+noOver.addEventListener("mouseover", reverseAns);
+yesOver.addEventListener("click", yesss);
 
-function reverseAns (){
-   answerBox[0].classList.toggle('row-reverse');
+function reverseAns() {
+  answerBox[0].classList.toggle("row-reverse");
 }
 
-function yesss (){
-    document.getElementById('music').play();
-    document.getElementById('iloveyou').style.display = 'block';
-    window.scrollTo(0,document.body.scrollHeight);
+function yesss() {
+  document.getElementById("music").play();
+  // window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0, document.body.scrollHeight);
 }
